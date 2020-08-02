3ff8793a100c75f356416868ccccadec114887ed
Hernan Saavedra Radovcic <nanoskull@gmail.com>
2020-08-02 12:44:13 -0400

    Added Commands
    - Added array of commands to the cat gif command
    - Added new array for a dog image command

--- test.js
+++ test.js
@@ -10,11 +10,18 @@ bot.command(['hola', 'wena'], message=> {
 	message.reply('Wena cabros');
 });
 
-bot.command('gato', async (message) =>{
+
+bot.command(['gato', 'cat', 'kitty', 'michi'], async (message) =>{
 	axios.defaults.headers.common['x-api-key'] = process.env.CAT_KEY;
 	let response = await axios.get('https://api.thecatapi.com/v1/images/search?mime_types=gif', { params: { limit:1, size:"full" } } )
 	this.image = response.data[0]
 	message.replyWithAnimation(this.image.url);
 });
+
+bot.command(['perro', 'goodboi', 'dog'], async (message) => {
+	let response = await axios.get('https://dog.ceo/api/breeds/image/random');
+	this.image = response.data;
+	message.replyWithPhoto(this.image.message);
+})
 
 bot.launch();
