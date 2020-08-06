0b8ae60ea92ce8b66db3f735af4f5c08d89f8f02
Franco Valerio <franco.gvp@hotmail.com>
2020-08-06 18:25:01 -0400

    Revert "Merge pull request #6 from francovp/random-meme"
    This reverts commit 5440cc0f098522fbb87901e03e6612c0d216a3bf.

--- test.js
+++ test.js
@@ -1,27 +1,12 @@
 const Telegraf = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TEST)
-const axios = require('axios');
+const Extra = require('telegraf/extra');
+const bot = new Telegraf(process.env.bot_test)
 
-bot.start(ctx => {
-	console.log('Inicio');
-})
-
-bot.command(['hola', 'wena'], message=> {
-	message.reply('Wena cabros');
+bot.command('hola', message=> {
+	message.reply('Hola Mundo');
 });
 
 
-bot.command(['gato', 'cat', 'kitty', 'michi'], async (message) =>{
-	axios.defaults.headers.common['x-api-key'] = process.env.CAT_KEY;
-	let response = await axios.get('https://api.thecatapi.com/v1/images/search?mime_types=gif', { params: { limit:1, size:"full" } } )
-	this.image = response.data[0]
-	message.replyWithAnimation(this.image.url);
 });
-
-bot.command(['perro', 'goodboi', 'dog'], async (message) => {
-	let response = await axios.get('https://dog.ceo/api/breeds/image/random');
-	this.image = response.data;
-	message.replyWithPhoto(this.image.message);
-})
 
 bot.launch();
