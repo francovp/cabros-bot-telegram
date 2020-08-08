66025ff61992aaf9971fd8b82f23280e97c936f5
Franco Valerio <franco.gvp@hotmail.com>
2020-08-07 21:41:28 -0400

    refactor(commands): reestructurar código

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Composer } = require('micro-bot');
 const Extra = require('telegraf/extra');
-const { catmeme } = require('./functions/commands');
+const { postMeme } = require('./functions/commands');
 const { getRandomMemeImage } = require('./functions/commons');
 const bot = new Composer;
 
@@ -8,28 +8,16 @@ bot.start(ctx => {
 	console.log('Inicio');
 });
 
-bot.command(['meme'], async (message) => {
-	try {
-		const response = await getRandomMemeImage('memes');
-		console.log('response: ', response);
-		message.replyWithPhoto(response);
-	}
-	catch (error) {
-		console.error(error);
-	}
+bot.command(['meme'], (context) => {
+	postMeme(context, 'memes');
 });
 
-bot.command(['dankmeme'], async (message) => {
-	try {
-		const response = await getRandomMemeImage('dankmemes');
-		console.log('response: ', response);
-		message.replyWithPhoto(response);
-	}
-	catch (error) {
-		console.error(error);
-	}
+bot.command(['dankmeme'], (context) => {
+	postMeme(context, 'dankmemes');
 });
 
-bot.command(['catmeme'], catmeme);
+bot.command(['catmeme'], (context) => {
+	postMeme(context, 'Catmemes');
+});
 
 module.exports = bot;
